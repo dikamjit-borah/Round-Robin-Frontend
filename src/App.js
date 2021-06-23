e8bf2916c97623b40c561f98c45751e5d0187834
Dikamjit Borah <learningdjb99@gmail.com>
2021-06-23 21:22:02 +0530

    set the static data for weekview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { React, useState, useEffect } from "react";
 import './App.scss';
-
+import Modal from "react-modal";
+import AddTopicComponent from "./components/AddTopicComponent/AddTopicComponent";
 import WeekComponent from './components/WeekComponent/WeekComponent';
 import NavigationBarComponent from './components/NavigationBarComponent/NavigationBarComponent';
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () =>{
+    setModalIsOpen(true)
+  }
   return (
+    
+    
     <div className="App">
+      
       <NavigationBarComponent></NavigationBarComponent>
+      
+      <button onClick={openModal}>Add Topic</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => {
+          setModalIsOpen(false);
+        }}
+        shouldCloseOnOverlayClick={true}
+      >
+        <AddTopicComponent startingTime={0}></AddTopicComponent>
+      </Modal>
       <div className="AppChild app-right">
        <WeekComponent></WeekComponent>
+
+       
       </div>
     </div>
   );
--- src/components/WeekComponent/WeekComponent.jsx
+++ src/components/WeekComponent/WeekComponent.jsx
@@ -2,38 +2,45 @@ import { React, useState, useEffect } from "react";
 import "./WeekComponent.scss";
 import Modal from "react-modal";
 import AddTopicComponent from "../AddTopicComponent/AddTopicComponent";
+import axios from 'axios';
+
 
 function WeekComponent() {
+  
+  const BASE_URL = "http://localhost:8080"
+  
   const [ids, setIds] = useState([1, 2, 3, 4, 5, 6, 7]);
   const [weekCount, setWeekCount] = useState(1);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  
   let data = [
     {
       schedule_id: 1,
-      scheduled_topic: "Topic Name",
+      scheduled_topic: "Marriage",
       scheduled_date: "3",
-      scheduled_start_time: "12:10",
-      scheduled_end_time: "14:10",
+      scheduled_start_time: "12",
+      scheduled_end_time: "14",
     },
     {
       schedule_id: 2,
-      scheduled_topic: "Topic Name",
+      scheduled_topic: "anniversaru",
       scheduled_date: "4",
-      scheduled_start_time: "11:00",
-      scheduled_end_time: "12:00",
+      scheduled_start_time: "11",
+      scheduled_end_time: "12",
     },
     {
       schedule_id: 3,
-      scheduled_topic: "Topic Name",
+      scheduled_topic: "Reception",
       scheduled_date: "7",
-      scheduled_start_time: "09:10",
-      scheduled_end_time: "15:10",
+      scheduled_start_time: "9",
+      scheduled_end_time: "15",
     },
     {
       schedule_id: 5,
-      scheduled_topic: "mmmm Name",
+      scheduled_topic: "Pokor",
       scheduled_date: "12",
-      scheduled_start_time: "09:10",
-      scheduled_end_time: "15:10",
+      scheduled_start_time: "12",
+      scheduled_end_time: "15",
     },
   ];
 
@@ -51,14 +58,59 @@ function WeekComponent() {
     }
   };
 
-  const setData = () => {
+  const setData = () => { 
+
+    axios.get(`${BASE_URL}/api/week_view`, {
+      params: {
+        teacher_id:"1624461442478",
+        week_start_date: '2021-02-01',
+        week_end_date: '2021-02-07'
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+
+    
     let colId = [];
+    let marginTop = [];
     for (let i = 0; i < data.length; i++) {
       colId.push(data[i]["scheduled_date"]);
+      marginTop.push(parseInt(data[i]["scheduled_start_time"] - 1)*42)
     }
+
+    console.log(marginTop);
+
+    for(let i = 0; i<24; i++)
+    {
+      let x = document.getElementById("time-column");
+      let y = `<div style="margin-top:${(marginTop[i])}px">${i+1}`
+    }
+
+    let timeColumnId = document.getElementById("time-column")
+    let timeColumnContent = ""
+
+    let j = 0
+    for(let i = 0; i<24; i++)
+    {
+      
+       timeColumnContent += `<p style="position:absolute;margin-top:${(j)}px">${i+1}</p>`
+       j+=42
+     
+    }
+
+
+
+    if (timeColumnId) timeColumnId.innerHTML = timeColumnContent ;
+
+
     for (let i = 0; i < colId.length; i++) {
       let x = document.getElementById(colId[i]);
-      if (x) x.innerHTML = data[i]["scheduled_topic"];
+     
+      let duration = parseInt(data[i]["scheduled_end_time"]) - parseInt(data[i]["scheduled_start_time"])
+      console.log(duration);
+      let y = `<div style="margin-top:${marginTop[i]}px; background-color:#bbdefb; height:${duration*42}px">${data[i]["scheduled_topic"]}</div>`
+      if (x) x.innerHTML = y ;
     }
   };
   const handleAllHr = () => {
@@ -91,7 +143,7 @@ function WeekComponent() {
     }
   };
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+
 
   const handleDayOne = (event) => {
     timeBasedOnClientY(event.pageY);
@@ -170,18 +222,27 @@ function WeekComponent() {
         <AddTopicComponent startingTime={startingTime}></AddTopicComponent>
       </Modal>
       <div className="calender-heading">
-        <div className="calender-header">1</div>
-        <div className="calender-header">2</div>
-        <div className="calender-header">3</div>
-        <div className="calender-header">4</div>
-        <div className="calender-header">5</div>
-        <div className="calender-header">6</div>
-        <div className="calender-header">7</div>
+      <div className="calender-header">Time</div>
+        <div className="calender-header">{ids[0]}</div>
+        <div className="calender-header">{ids[1]}</div>
+        <div className="calender-header">{ids[2]}</div>
+        <div className="calender-header">{ids[3]}</div>
+        <div className="calender-header">{ids[4]}</div>
+        <div className="calender-header">{ids[5]}</div>
+        <div className="calender-header">{ids[6]}</div>
       </div>
 
       {handleAllHr()}
 
       <div className="calender-columns">
+
+        
+      <div
+          className="calender-column"
+          id="time-column"
+         
+        ></div>
+        
         <div
           className="calender-column"
           id={ids[0]}
